48071e2f022a3a7201dee705d6f7770a59403697
Adrian Rincon Lopez <adrianrl9441@gmail.com>
2017-10-29 19:52:53 +0100

    Building animal lists

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales'
+import { Animal } from '../../interfaces/animal.interface'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
-
+  animales:Animal[] = [];
   constructor(public navCtrl: NavController) {
-
+    this.animales = ANIMALES.splice(0);
   }
-
+  play(animal: Animal) {
+    console.log('animal', animal);
+    let audio = new Audio();
+    audio.src = animal.audio;
+    animal.reproduciendo = true;
+    if(animal.reproduciendo) {
+      audio.load();
+      audio.play();
+      setTimeout( () => animal.reproduciendo = false, animal.duracion * 1000);
+    }
+  }
 }
